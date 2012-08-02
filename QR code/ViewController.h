dda8075b1e48ca83387d97d1fa5e8fe05e44bb02
斌 <Bear@bindeMacBook-Pro.local>
2012-08-02 17:52:58 +0800

    Initial Commit

---
+++ QR code/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  QR code
+//
+//  Created by 斌 on 12-8-2.
+//  Copyright (c) 2012年 斌. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
